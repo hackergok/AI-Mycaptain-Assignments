9a703d8bee28a516fc50d52cbecefbb39a1efd42
Gokul kamalesh <105118149+hackergok@users.noreply.github.com>
2022-06-25 19:57:05 +0530

    Add files via upload

---
+++ postive.py
@@ -0,0 +1,10 @@
+list1 = [11, -21, 0, 45, 66, -93]
+list2 = [12, 14, -95, 3] 
+
+for num in list1:
+     if num >= 0:
+       print(num, end = " ")
+
+for num in list2:
+     if num >= 0:
+       print(num, end = "")
